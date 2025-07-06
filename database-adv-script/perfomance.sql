4c6bcf03563a266a5ad9fe0d5e44dfbd7effe029
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-07-06 11:26:22 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,19 +1,19 @@
 -- Unoptimized complex query
+-- Analyze performance before optimization
+EXPLAIN
 SELECT 
-  u.user_id,
-  u.first_name,
-  u.email,
-  b.booking_id,
-  b.start_date,
-  b.end_date,
-  b.status,
-  p.property_id,
-  p.name AS property_name,
-  p.pricepernight,
-  pay.payment_id,
-  pay.amount,
-  pay.payment_method
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.first_name,
+    u.last_name,
+    p.name AS property_name,
+    pay.amount,
+    pay.payment_method
 FROM Booking b
 JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON pay.booking_id = b.booking_id;
+JOIN Payment pay ON pay.booking_id = b.booking_id
+WHERE b.status = 'confirmed' AND b.total_price > 100;
